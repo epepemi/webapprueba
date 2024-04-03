75c1499076dc62275383c957ff445e0b549a020d
epepemi <842574@unizar.es>
2024-04-03 13:13:24 +0200

    Update link text in App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aplendele al react coooooooñocoño
         </a>
       </header>
     </div>
