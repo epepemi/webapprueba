d34bd499814386a8d5d8897c2e9d7ff80d5de867
epepemi <842574@unizar.es>
2024-04-09 12:34:36 +0200

    prueba2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aplendele al react por favor muchas gracias
+          Aplendele al react por favor muchas gracias porfavor
         </a>
       </header>
     </div>
