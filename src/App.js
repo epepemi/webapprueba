9692d021fad7bd09d05740aed3ef76f7b3d6c9e7
epepemi <842574@unizar.es>
2024-04-09 12:54:46 +0200

    lastitesti

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aplendele al react por favor muchas gracias porfavor
+          Aplendele al react ostias
         </a>
       </header>
     </div>
